d0410225c798784e42d4c551a829b7690926b93e
Prasad Surase <prasad.surase@coupa.com>
2021-05-01 12:17:44 +0530

    faq app

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = (props) => {
+  const {title, info} = props;
+  const [active, setActive] = useState(false);
+
+  const renderButton = () => {
+    if(active) {
+      return (
+        <button className="btn" onClick={() => setActive(false)}>
+          <AiOutlineMinus />
+        </button>
+      );
+    } else {
+      return (
+        <button className="btn" onClick={() => setActive(true)}>
+          <AiOutlinePlus />
+        </button>
+      );
+    }
+  };
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        {renderButton()}
+      </header>
+      { active && <p>{info}</p> }
+    </article>
+  );
 };
 
 export default Question;
